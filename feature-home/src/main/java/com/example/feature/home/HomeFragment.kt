935d7c31d9ea279bef5fc251fcae4941a61f707b
ifvodeky24 <ifvodeky.w24@gmail.com>
2022-04-27 06:59:09 +0700

    AAdd banner on home

--- feature-home/src/main/java/com/example/feature/home/HomeFragment.kt
+++ feature-home/src/main/java/com/example/feature/home/HomeFragment.kt
@@ -1,15 +1,24 @@
 package com.example.feature.home
 
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager.widget.ViewPager
 import com.example.core_data.api.ApiEvent
 import com.example.core_navigation.ModuleNavigator
 import com.example.core_resource.components.base.BaseFragment
 import com.example.core_util.gone
 import com.example.core_util.visible
 import com.example.feature.auth.AuthViewModel
+import com.example.home.R
 import com.example.home.databinding.FragmentHomeBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import java.util.*
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate),
     ModuleNavigator {
@@ -19,33 +28,120 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     private var level = ""
     private var userId = ""
 
+    private var dotscount: Int = 0
+    private var dots: Array<ImageView?>? = null
+    private var viewPagerAdapter: BannerViewPagerAdapter? = null
+    private var timer: Timer? = null
+
     override fun initView() {
         observeAuth()
+        initImage()
+    }
+
+    private fun initImage() {
+        viewPagerAdapter = BannerViewPagerAdapter(requireActivity())
+        binding.vpGaleri.adapter = viewPagerAdapter
+
+        viewPagerAdapter.let { viewPagerAdapter ->
+            if (viewPagerAdapter != null) {
+                dotscount = viewPagerAdapter.count
+            }
+        }
+
+        dots = arrayOfNulls(dotscount)
+
+        val params = LinearLayoutCompat.LayoutParams(
+            LinearLayoutCompat.LayoutParams.WRAP_CONTENT,
+            LinearLayoutCompat.LayoutParams.WRAP_CONTENT
+        )
+        params.marginStart = 8
+        params.marginEnd = 8
+        params.setMargins(8, 0, 8, 0)
+
+        if (dots != null) {
+            for (i in dots!!.indices) {
+                val img = ImageView(requireActivity())
+                dots!![i] = img
+                dots!![i]?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireActivity(),
+                        R.drawable.nonactive_dot
+                    )
+                )
+                binding.sliderDots.addView(dots!![i], params)
+            }
+
+            dots!![0]?.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireActivity(),
+                    R.drawable.active_dot
+                )
+            )
+        }
+
+        binding.vpGaleri.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+
+                if (dots != null) {
+                    for (i in dots!!.indices) {
+                        dots!![i]?.setImageDrawable(
+                            ContextCompat.getDrawable(
+                                requireActivity(),
+                                R.drawable.nonactive_dot
+                            )
+                        )
+                    }
+
+                    dots!![position]?.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireActivity(),
+                            R.drawable.active_dot
+                        )
+                    )
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
+
+//        timer = Timer()
+//        timer?.scheduleAtFixedRate(MyTimerTask(), 2000, 8000)
     }
 
     override fun initListener() {
-        with(binding){
-            cvPakar.setOnClickListener {
+        with(binding) {
+            ivPakar.setOnClickListener {
                 viewModel.getLastResult(userId.toInt())
                 viewModel.lastResultAll.observe(viewLifecycleOwner) { hasilAngket ->
                     when (hasilAngket) {
                         is ApiEvent.OnSuccess -> {
                             hasilAngket.getData()?.let {
                                 if (it.isNullOrEmpty()) navigateToPakarActivity()
-                                else Toast.makeText(requireContext(), "Data sudah ada", Toast.LENGTH_SHORT).show()
+                                else Toast.makeText(
+                                    requireContext(),
+                                    "Data sudah ada",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             } ?: navigateToPakarActivity()
                         }
                     }
                 }
             }
-            cvHasilAngket.setOnClickListener {
+            ivHasilAngket.setOnClickListener {
                 navigateToHasilAngketActivity()
             }
 
-            cvNilaiSiswa.setOnClickListener {
+            ivNilaiSiswa.setOnClickListener {
                 navigateToNilaiSiswaActivity(level = level)
             }
-            cvHasilPenjurusan.setOnClickListener {
+            ivHasilPenjurusan.setOnClickListener {
                 navigateToNilaiPenjurusanActivity(level = level, userId = userId)
             }
 
@@ -72,5 +168,29 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
             }
         }
 
+    }
+
+    inner class MyTimerTask : TimerTask() {
+        override fun run() {
+            CoroutineScope(Dispatchers.Main).launch {
+                when (binding.vpGaleri.currentItem) {
+                    0 -> binding.vpGaleri.currentItem = 1
+                    1 -> binding.vpGaleri.currentItem = 2
+                    2 -> binding.vpGaleri.currentItem = 3
+                    3 -> binding.vpGaleri.currentItem = 4
+                    4 -> binding.vpGaleri.currentItem = 5
+                    5 -> binding.vpGaleri.currentItem = 6
+                    6 -> binding.vpGaleri.currentItem = 7
+                    else -> binding.vpGaleri.currentItem = 0
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        dots = null
+        viewPagerAdapter = null
+        timer = null
     }
 }
